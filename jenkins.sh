e435b2ff39847f5475459333cd83f4d053429641
21169420 <21169420>
2023-03-24 20:23:07 +0000

    docker project

---
+++ jenkins.sh
@@ -0,0 +1,10 @@
+
+  yum install docker -y
+  systemctl start docker
+  sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+  sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+  amazon-linux-extras install java-openjdk11 -y
+  yum install git jenkins -y
+  systemctl start jenkins
+  systemctl status jenkins
+
